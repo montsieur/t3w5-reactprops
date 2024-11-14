7aa7b33e98ba68899d988fa9d162414e9c60a194
montsieur <dndott@gmail.com>
2024-11-14 21:31:48 +1100

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
-export default function Footer(){
+export default function Header(){
     return (
-        <p><b>CoderAcademy 2024.</b></p>
+        <h1>React Props Lesson</h1>
     )
 }
+
+// module.exports = Header;
